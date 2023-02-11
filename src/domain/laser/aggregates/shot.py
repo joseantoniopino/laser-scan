7477d8de79bdffcf2fae4af5674778e1c287946d
Jose A <josanpino@gmail.com>
2023-02-11 19:38:10 +0100

    feature(Laser): refactor to separate firing responsibility into another class

---
+++ src/domain/laser/aggregates/shot.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timedelta
+from math import sqrt
+
+from src.domain.common.faction import Faction
+from src.domain.common.value_objects import Coordinates
+
+
+class Shot:
+    def __init__(self, origin: Coordinates, target: Target):
+        self._origin = origin
+        self._last_fire = None
+        self._target = target
+        self._target_position = target.get_position()
+        self._distance = self._calculate_distance()
+        self._reload_time = self._calculate_reload_time()
+
+    def fire(self):
+        if not self._target:
+            raise Exception("No hay blanco seleccionado.")
+        if self._target.get_faction() == Faction.REBEL.value:
+            raise Exception("No se puede apuntar a un blanco amigo.")
+        if self._last_fire and not self._can_fire():
+            raise Exception("El laser aún no se ha recargado.")
+        reload_time = self._reload_time
+        self._last_fire = datetime.now()
+        print(f"El laser ha sido disparado desde {self._origin} hacia el blanco {self._target.get_name()} en {self._target.get_position()}. Tiempo de recarga: {reload_time}")
+
+    def _can_fire(self):
+        return datetime.now() >= self._last_fire + self._reload_time()
+
+    def _calculate_reload_time(self) -> timedelta:
+        if self._distance < 10:
+            return timedelta(seconds=30)
+        elif 10 <= self._distance < 50:
+            return timedelta(seconds=60)
+        elif 50 <= self._distance <= 100:
+            return timedelta(seconds=105)
+        else:
+            raise Exception("El laser solo puede disparar hasta 100 unidades astronómicas.")
+
+    def _calculate_distance(self) -> float:
+        origin_x = self._origin.get_x()
+        origin_y = self._origin.get_y()
+        target_x = self._target_position.get_x()
+        target_y = self._target_position.get_y()
+        return sqrt((target_x - origin_x) ** 2 + (target_y - origin_y) ** 2)
--- src/domain/laser/laser.py
+++ src/domain/laser/laser.py
@@ -1,19 +1,13 @@
-from math import sqrt
-from datetime import datetime, timedelta
-
 from src.domain.common.value_objects import Coordinates
-from src.domain.common.faction import Faction
+from src.domain.laser.aggregates.shot import Shot
 from src.domain.target.target import Target
 
 
 class Laser:
     def __init__(self, origin: Coordinates, target: Target):
         self._origin = origin
-        self._last_fire = None
         self._target = target
-        self._target_position = target.get_position()
-        self._distance = self._calculate_distance()
-        self._reload_time = self._calculate_reload_time()
+        self._shot = Shot(self._origin, self._target)
 
     def get_origin(self):
         return self._origin
@@ -25,36 +19,7 @@ class Laser:
         return self._target
 
     def fire(self):
-        if not self._target:
-            raise Exception("No hay blanco seleccionado.")
-        if self._target.get_faction() == Faction.REBEL.value:
-            raise Exception("No se puede apuntar a un blanco amigo.")
-        if self._last_fire and not self._can_fire():
-            raise Exception("El laser aún no se ha recargado.")
-        reload_time = self._reload_time
-        self._last_fire = datetime.now()
-        print(f"El laser ha sido disparado desde {self._origin} hacia el blanco {self._target.get_name()} en {self._target.get_position()}. Tiempo de recarga: {reload_time}")
-
-    def _can_fire(self):
-        return datetime.now() >= self._last_fire + self._reload_time()
-
-    def _calculate_distance(self) -> float:
-        origin_x = self._origin.get_x()
-        origin_y = self._origin.get_y()
-        target_x = self._target_position.get_x()
-        target_y = self._target_position.get_y()
-        return sqrt((target_x - origin_x) ** 2 + (target_y - origin_y) ** 2)
-
-    @staticmethod
-    def _calculate_reload_time(self) -> timedelta:
-        if self._distance < 10:
-            return timedelta(seconds=30)
-        elif 10 <= self._distance < 50:
-            return timedelta(seconds=60)
-        elif 50 <= self._distance <= 100:
-            return timedelta(seconds=105)
-        else:
-            raise Exception("El laser solo puede disparar hasta 100 unidades astronómicas.")
+        return self._shot.fire()
 
     def __str__(self):
         return f"Laser(origin={self._origin}, target={self._target}, last_fire={self._last_fire})"
