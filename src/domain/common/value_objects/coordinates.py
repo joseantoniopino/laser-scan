7bac968609fc3de5638b9f00033e795ecb3d8617
Jose A <josanpino@gmail.com>
2023-02-11 03:59:44 +0100

    feature(coordinates): add getters for private properties

--- src/domain/common/value_objects/coordinates.py
+++ src/domain/common/value_objects/coordinates.py
@@ -1,15 +1,21 @@
 class Coordinates:
     def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
 
     def __eq__(self, other):
         if isinstance(other, Coordinates):
-            return self.x == other.x and self.y == other.y
+            return self._x == other._x and self._y == other._y
         return False
 
     def __str__(self):
-        return f"({self.x}, {self.y})"
+        return f"({self._x}, {self._y})"
 
     def __repr__(self):
         return self.__str__()
